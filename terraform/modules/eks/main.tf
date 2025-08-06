f3b1d70bf6b0693c3c07c4ddb5431d9475c9d439
jmcastellanojimenez <jmcastellanojimenez@yahoo.es>
2025-08-06 20:44:55 +0200

    node group subnets private to public to enable internet without NAT Gtw

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -177,7 +177,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.cluster_name}-nodes"
   node_role_arn   = var.node_group_role_arn
-  subnet_ids      = var.private_subnet_ids
+  subnet_ids      = var.public_subnet_ids
 
   capacity_type  = var.capacity_type
   instance_types = var.instance_types
