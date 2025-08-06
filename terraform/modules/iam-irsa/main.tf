366f036e65b9524708f7c42ffd6fde0e23a24818
jmcastellanojimenez <jmcastellanojimenez@yahoo.es>
2025-08-06 23:11:25 +0200

    Changed from Amazon_EBS_CSI_DriverPolicy to AmazonEBSCSIDriverPolicy (AWS updated the policy name)

--- terraform/modules/iam-irsa/main.tf
+++ terraform/modules/iam-irsa/main.tf
@@ -29,6 +29,6 @@ resource "aws_iam_role" "ebs_csi_driver" {
 }
 
 resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/Amazon_EBS_CSI_DriverPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.ebs_csi_driver.name
 }
